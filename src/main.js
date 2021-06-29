71513b117f4765fc7518f0818c8fdc0095dc36c8
Evan Lake <evanlake583@gmail.com>
2021-06-29 08:57:19 -0700

    added new messeges in main file

--- src/main.js
+++ src/main.js
@@ -12,3 +12,15 @@ let keyF, keyR, keyLEFT, keyRIGHT;
 //set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+
+/*
+--------POINT BREAKDOWN---------
+60 - new non-scifi theme
+20 - new enemy that gives you time (the rare worm)
+10 - display the time
+10 - animated sprites for all the enemies
+
+Evan Lake
+Mole Patrol
+6/29/2021
+3 days to complete (1 day for Design, 2 days of programming)
